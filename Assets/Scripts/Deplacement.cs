d41ecbe538bdf7d4f182e517f408c012ea05d230
Axel Guignard <axelguignard@yahoo.fr>
2017-11-24 23:23:24 +0100

    Ajout du déplacement de base du personnage

---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour {
+
+    float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        if (inputX != 0 || inputY != 0)
+        {
+            transform.Translate(speed * inputX, speed * inputY, 0);
+        }
+    }
+}
