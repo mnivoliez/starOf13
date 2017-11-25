95bc0de87c25a14a51e523b6ff3966c6b66f7f0d
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-11-25 20:48:36 +0100

    fix typo

--- Assets/Scripts/Deplacement.cs
+++ Assets/Scripts/Deplacement.cs
@@ -4,62 +4,41 @@ using UnityEngine;
 
 public class Deplacement : MonoBehaviour {
 
-    [SerializeField] private float speed = 0.1f;
-    private Vector2 mvt;
-    private float inputX;
-    private float inputY;
+	[SerializeField] private float speed = 0.1f;
+	private Vector2 mvt;
+	private float inputX;
+	private float inputY;
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
-        mvt = new Vector2(speed * inputX, speed * inputY);
-    }
+		inputX = Input.GetAxisRaw ("Horizontal");
+		inputY = Input.GetAxisRaw ("Vertical");
+		mvt = new Vector2 (speed * inputX, speed * inputY);
+	}
 
-    void FixedUpdate()
-    {
-<<<<<<< HEAD
-        this.GetComponent<Rigidbody2D>().velocity = mvt;
-        //transform.Translate(mvt.x, mvt.y, 0);
-=======
-        GetComponent<Rigidbody2D>().velocity = mvt;
->>>>>>> ajout path system NPC
-        if (inputX == 0 && inputY > 0)
-        {
-            GetComponent<Direction>().Set(1);
-        }
-        else if (inputX > 0 && inputY > 0)
-        {
-            GetComponent<Direction>().Set(2);
-        }
-        else if (inputX > 0 && inputY == 0)
-        {
-            GetComponent<Direction>().Set(3);
-        }
-        else if (inputX > 0 && inputY < 0)
-        {
-            GetComponent<Direction>().Set(4);
-        }
-        else if (inputX == 0 && inputY < 0)
-        {
-            GetComponent<Direction>().Set(5);
-        }
-        else if (inputX < 0 && inputY < 0)
-        {
-            GetComponent<Direction>().Set(6);
-        }
-        else if (inputX < 0 && inputY == 0)
-        {
-            GetComponent<Direction>().Set(7);
-        }
-        else if (inputX < 0 && inputY > 0)
-        {
-            GetComponent<Direction>().Set(8);
-        }
-    }
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity = mvt;
+		if (inputX == 0 && inputY > 0) {
+			GetComponent<MDirection> ().Set (1);
+		} else if (inputX > 0 && inputY > 0) {
+			GetComponent<MDirection> ().Set (2);
+		} else if (inputX > 0 && inputY == 0) {
+			GetComponent<MDirection> ().Set (3);
+		} else if (inputX > 0 && inputY < 0) {
+			GetComponent<MDirection> ().Set (4);
+		} else if (inputX == 0 && inputY < 0) {
+			GetComponent<MDirection> ().Set (5);
+		} else if (inputX < 0 && inputY < 0) {
+			GetComponent<MDirection> ().Set (6);
+		} else if (inputX < 0 && inputY == 0) {
+			GetComponent<MDirection> ().Set (7);
+		} else if (inputX < 0 && inputY > 0) {
+			GetComponent<MDirection> ().Set (8);
+		}
+	}
 }
