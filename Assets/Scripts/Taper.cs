550007f92d9ad8c60b5b4579f3a5e6d83997f2f9
Axel Guignard <axelguignard@yahoo.fr>
2017-11-25 12:39:34 +0100

    knockback 8 directions

--- Assets/Scripts/Taper.cs
+++ Assets/Scripts/Taper.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Taper : MonoBehaviour
 {
-
     bool hitting = false;
     BoxCollider2D hit = null;
+    float knockback = 0.3f;
+    float knockbackDiago = 0.15f;
 
     // Use this for initialization
     void Start()
@@ -21,11 +22,49 @@ public class Taper : MonoBehaviour
         {
             if (Input.GetButtonDown("Fire1"))
             {
+                Vector2 charSize = GetComponent<Renderer>().bounds.size;
+
                 hit = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
                 hit.isTrigger = true;
-                hit.size = new Vector2(0.1f, GetComponent<Renderer>().bounds.size.y);
 
-                hit.offset = new Vector2(GetComponent<Renderer>().bounds.size.x, 0);
+                switch (GetComponent<Direction>().Get())
+                {
+                    case 1:
+                        hit.size = new Vector2(charSize.x, 0.1f);
+                        hit.offset = new Vector2(0, charSize.y);
+                        break;
+                    case 2:
+                        hit.size = new Vector2(0.1f, 0.1f);
+                        hit.offset = new Vector2(charSize.x, charSize.y);
+                        break;
+                    case 3:
+                        hit.size = new Vector2(0.1f, charSize.y);
+                        hit.offset = new Vector2(charSize.x, 0);
+                        break;
+                    case 4:
+                        hit.size = new Vector2(0.1f, 0.1f);
+                        hit.offset = new Vector2(charSize.x, -charSize.y);
+                        break;
+                    case 5:
+                        hit.size = new Vector2(charSize.y, 0.1f);
+                        hit.offset = new Vector2(0, -charSize.y);
+                        break;
+                    case 6:
+                        hit.size = new Vector2(0.1f, 0.1f);
+                        hit.offset = new Vector2(-charSize.x, -charSize.y);
+                        break;
+                    case 7:
+                        hit.size = new Vector2(0.1f, charSize.y);
+                        hit.offset = new Vector2(-charSize.x, 0);
+                        break;
+                    case 8:
+                        hit.size = new Vector2(0.1f, 0.1f);
+                        hit.offset = new Vector2(-charSize.x, charSize.y);
+                        break;
+                    default:
+                        break;
+                }
+                
                 hitting = true;
             }
         }
@@ -41,7 +80,35 @@ public class Taper : MonoBehaviour
 
         if (other.GetComponent<Hittable>() as Hittable != null && hitting)
         {
-            other.gameObject.transform.Translate(0.3f, 0, 0);
+            switch (GetComponent<Direction>().Get())
+            {
+                case 1:
+                    other.gameObject.transform.Translate(0, knockback, 0);
+                    break;
+                case 2:
+                    other.gameObject.transform.Translate(knockbackDiago, knockbackDiago, 0);
+                    break;
+                case 3:
+                    other.gameObject.transform.Translate(knockback, 0, 0);
+                    break;
+                case 4:
+                    other.gameObject.transform.Translate(knockbackDiago, -knockbackDiago, 0);
+                    break;
+                case 5:
+                    other.gameObject.transform.Translate(0, -knockback, 0);
+                    break;
+                case 6:
+                    other.gameObject.transform.Translate(-knockbackDiago, -knockbackDiago, 0);
+                    break;
+                case 7:
+                    other.gameObject.transform.Translate(-knockback, 0, 0);
+                    break;
+                case 8:
+                    other.gameObject.transform.Translate(-knockbackDiago, knockbackDiago, 0);
+                    break;
+                default:
+                    break;
+            }
             Destroy(hit);
             hitting = false;
         }
