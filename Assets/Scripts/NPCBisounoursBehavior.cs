7c5203fa55a3ced7f2ba2611928203b71fc2a529
Axel Guignard <axelguignard@yahoo.fr>
2017-11-25 23:27:09 +0100

    correction des paths, changement de behavior de déplacement sur changement de phase

--- Assets/Scripts/NPCBisounoursBehavior.cs
+++ Assets/Scripts/NPCBisounoursBehavior.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCBisounoursBehavior : MonoBehaviour {
-
 	private FollowPath[] _FollowPaths;
 
 	// Use this for initialization
