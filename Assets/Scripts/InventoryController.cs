a8184520f047b8c90d47ada867fc0ba9e3053bef
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-11-25 12:13:05 +0100

    add stand. Will look forward to a shop manager to make stand popup

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -17,21 +17,28 @@ public class InventoryController : MonoBehaviour {
 		
 	}
 
-	void TryBuyItem (ItemBehavior item) {
-		if (item.GetPrice () <= _money) {
+	void TryBuyItem (ShopItemStandBehavior item) {
+		if (CanBuy (item)) {
 			switch (item.GetItemType ()) {
 			case ItemType.FOOD:
 				_slot_health++;
+				item.TakeOne ();
 				_money -= item.GetPrice ();
 				break;
 			case ItemType.BASEBALL_BAT:
 				if (!_has_baseball_bat) {
 					_money -= item.GetPrice ();
+					item.TakeOne ();
 					_has_baseball_bat = true;
 				} else {
 					Debug.Log ("Already has a bat");
 				}			
+				break;
 			}
 		}
+	}
+
+	private bool CanBuy (ShopItemStandBehavior item) {
+		return item.GetStock () > 0 && item.GetPrice () >= _money;
 	}
 }
