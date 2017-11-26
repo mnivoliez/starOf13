1a2e72976c972186362aeae84cb863d6a727cd92
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-11-26 15:04:34 +0100

    hey

--- Assets/Scripts/Taper.cs
+++ Assets/Scripts/Taper.cs
@@ -2,89 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Taper : MonoBehaviour
-{
-    bool hitting = false;
-    BoxCollider2D hit = null;
-    float knockback = 0.3f;
+public class Taper : MonoBehaviour {
+	bool hitting = false;
+	BoxCollider2D hit = null;
+	float knockback = 0.3f;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!hitting)
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                GetComponent<Animator>().SetBool("isHitting", true);
+	// Update is called once per frame
+	void Update () {
+		if (!hitting) {
+			if (Input.GetButtonDown ("Fire1")) {
+				GetComponent<Animator> ().SetBool ("isHitting", true);
 
-                Vector2 charSize = GetComponent<Renderer>().bounds.size;
+				Vector2 charSize = GetComponent<Renderer> ().bounds.size;
 
-                hit = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
-                hit.isTrigger = true;
+				hit = gameObject.AddComponent<BoxCollider2D> () as BoxCollider2D;
+				hit.isTrigger = true;
 
-                switch (GetComponent<MDirection>().Get())
-                {
-                    case Direction.TOP:
-                        hit.size = new Vector2(charSize.x, 0.1f);
-                        hit.offset = new Vector2(0, charSize.y);
-                        break;
-                    case Direction.RIGHT:
-                        hit.size = new Vector2(0.1f, charSize.y);
-                        hit.offset = new Vector2(charSize.x, 0);
-                        break;
-                    case Direction.BOTTOM:
-                        hit.size = new Vector2(charSize.y, 0.1f);
-                        hit.offset = new Vector2(0, -charSize.y);
-                        break;
-                    case Direction.LEFT:
-                        hit.size = new Vector2(0.1f, charSize.y);
-                        hit.offset = new Vector2(-charSize.x, 0);
-                        break;
-                    default:
-                        break;
-                }
+				switch (GetComponent<MDirection> ().Get ()) {
+				case Direction.TOP:
+					hit.size = new Vector2 (charSize.x, 0.1f);
+					hit.offset = new Vector2 (0, charSize.y);
+					break;
+				case Direction.RIGHT:
+					hit.size = new Vector2 (0.1f, charSize.y);
+					hit.offset = new Vector2 (charSize.x, 0);
+					break;
+				case Direction.BOTTOM:
+					hit.size = new Vector2 (charSize.y, 0.1f);
+					hit.offset = new Vector2 (0, -charSize.y);
+					break;
+				case Direction.LEFT:
+					hit.size = new Vector2 (0.1f, charSize.y);
+					hit.offset = new Vector2 (-charSize.x, 0);
+					break;
+				default:
+					break;
+				}
 
-                hitting = true;
-            }
-        }
-        else if (hit != null)
-        {
-            Destroy(hit);
-            hitting = false;
-            GetComponent<Animator>().SetBool("isHitting", false);
-        }
-    }
+				hitting = true;
+			}
+		} else if (hit != null) {
+			Destroy (hit);
+			hitting = false;
+			GetComponent<Animator> ().SetBool ("isHitting", false);
+		}
+	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
+	void OnTriggerEnter2D (Collider2D other) {
 
-        if (other.GetComponent<Hittable>() as Hittable != null && hitting)
-        {
-            switch (GetComponent<MDirection>().Get())
-            {
-                case Direction.TOP:
-                    other.gameObject.transform.Translate(0, knockback, 0);
-                    break;
-                case Direction.RIGHT:
-                    other.gameObject.transform.Translate(knockback, 0, 0);
-                    break;
-                case Direction.BOTTOM:
-                    other.gameObject.transform.Translate(0, -knockback, 0);
-                    break;
-                case Direction.LEFT:
-                    other.gameObject.transform.Translate(-knockback, 0, 0);
-                    break;
-                default:
-                    break;
-            }
-            Destroy(hit);
-            hitting = false;
-        }
-    }
+		if (other.GetComponent<Hittable> () != null && hitting) {
+			switch (GetComponent<MDirection> ().Get ()) {
+			case Direction.TOP:
+				other.gameObject.transform.Translate (0, knockback, 0);
+				break;
+			case Direction.RIGHT:
+				other.gameObject.transform.Translate (knockback, 0, 0);
+				break;
+			case Direction.BOTTOM:
+				other.gameObject.transform.Translate (0, -knockback, 0);
+				break;
+			case Direction.LEFT:
+				other.gameObject.transform.Translate (-knockback, 0, 0);
+				break;
+			default:
+				break;
+			}
+			Destroy (hit);
+			hitting = false;
+		}
+	}
 }
