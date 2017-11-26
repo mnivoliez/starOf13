ec3e5e2e55fecd12029dd9334a8a4d8a3bf542f9
Axel Guignard <axelguignard@yahoo.fr>
2017-11-26 13:26:17 +0100

    Ajout interaction PNJ Bisounours qui fonctionne

--- Assets/Scripts/NPCBisounoursBehavior.cs
+++ Assets/Scripts/NPCBisounoursBehavior.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
-public class NPCBisounoursBehavior : MonoBehaviour {
+public class NPCBisounoursBehavior : MonoBehaviour, IInteractable {
 	private FollowPath[] _FollowPaths;
 
 	// Use this for initialization
 	void Start () {
 		_FollowPaths = GetComponentsInParent<FollowPath> ();
-	}
+    }
 
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+    public void Interact(GameObject player)
+    {
+        //GameObject.FindGameObjectWithTag("Audio").GetComponent<>().Play("nom_du_son/musique");
+        int dialogueNumber = Random.Range(1, 4);
+
+        switch (dialogueNumber)
+        {
+            case 1:
+                GameObject.FindGameObjectWithTag("Text_dial").GetComponent<Text>().text = "Cas 1";
+                break;
+            case 2:
+                GameObject.FindGameObjectWithTag("Text_dial").GetComponent<Text>().text = "Cas 2";
+                break;
+            case 3:
+                GameObject.FindGameObjectWithTag("Text_dial").GetComponent<Text>().text = "Cas 3";
+                break;
+            case 4:
+                GameObject.FindGameObjectWithTag("Text_dial").GetComponent<Text>().text = "Cas 4";
+                break;
+        }
+    }
 
 	public void EnterPhase (Phase phase) {
 		switch (phase) {
