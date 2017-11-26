678de629a4d1068a92e2a1be6690d974c39ac6f8
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-11-26 14:35:29 +0100

    Accordéon

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+
+public class AudioManager : MonoBehaviour, IPhaseDriven {
+	[SerializeField]
+	public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+		foreach (Sound s in sounds) {
+			s.source = gameObject.AddComponent<AudioSource> ();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+		}
+
+	}
+
+	void Start () {
+		Play ("Music_Normal");
+	}
+
+
+	public void Play (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		if (s == null) {
+			Debug.LogWarning ("!!!!!!!!!!!!!! Fichier " + name + " non trouvé !!!!!!!!!!!!");
+			return;
+		}
+		s.source.Play ();
+	}
+
+	public void Stop (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		if (s == null) {
+			Debug.LogWarning ("!!!!!!!!!!!!!! Fichier " + name + " non trouvé !!!!!!!!!!!!");
+			return;
+		}
+		s.source.Stop ();
+	}
+
+	// Switch music
+
+	#region IPhaseDriven implementation
+
+	public void EnterPhase (Phase phase) {
+		switch (phase) {
+		case Phase.BISOUNOURS:
+			Stop ("Music_V13");
+			Play ("Music_Normal");
+			break;
+		case Phase.FRIDAY_13:
+			Stop ("Music_Normal");
+			Play ("Music_V13");
+			break;
+		}
+	}
+
+	#endregion
+}
--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -9,9 +9,9 @@ public class Sound {
 
 	public AudioClip clip;
 
-	[Range(0f, 1f)]
+	[Range (0f, 1f)]
 	public float volume;
-	[Range(0.1f, 3f)]
+	[Range (0.1f, 3f)]
 	public float pitch;
 
 	public bool loop;
